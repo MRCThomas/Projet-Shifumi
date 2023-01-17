c817146cb0cea56456089deb12fe5cf5f122dc81
Thomas Marchi <thomas.marchi@orange.fr>
2023-01-17 09:53:59 +0100

    commit tout petit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let chooseCharater = document.querySelector(".");
+let chooseWeapon = document.querySelector(".");
+
+
+chooseCharater.addEventListener("mouseover", selectCharater);
+
+function selectCharater() {
+    
+}
+chooseWeapon.addEventListener("click", selectWeapon);
+
+function selectWeapon() {
+    
+}
